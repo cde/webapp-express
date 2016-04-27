cb48144a42b0df5223532895ed4dcdc62202518b
cde <cdechauri@gmail.com>
2016-04-27 12:25:55 -0700

    Example with handlebars template engine

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-
 app.set('views','./src/views');
-app.set('view engine','jade');
+
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine','.hbs');
 
 app.get('/', function(req, res) {
-    res.render('index', {list:['Carmen', 'Gabriel', 'Jeremy']});
+    res.render('index', {title: 'Render from handlebars', list:['Carmen', 'Gabriel', 'Jeremy']});
 });
 app.listen(5000, function(err) {
     console.log('running server on port ' + port);
