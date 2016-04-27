4907158aab2b103d7bd9012b0d36b4c022141c53
cde <cdechauri@gmail.com>
2016-04-27 12:11:53 -0700

    Example with jade template engine

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+
+app.set('views','./src/views');
+app.set('view engine','jade');
 
 app.get('/', function(req, res) {
-    res.send('Hello Word');
+    res.render('index', {list:['Carmen', 'Gabriel', 'Jeremy']});
 });
 app.listen(5000, function(err) {
     console.log('running server on port ' + port);
