39811bf03977a33d373d77d409746d32ea49c85a
cde <cdechauri@gmail.com>
2016-04-27 13:52:40 -0700

    Add routes for books

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
+var bookRouter = express.Router();
+
 
 app.use(express.static('public'));
 app.set('views','./src/views');
 
-var handlebars = require('express-handlebars');
-app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine','ejs');
 
-app.set('view engine','.hbs');
+bookRouter.route('/')
+    .get(function(req,res){
+        res.send('Hello Books');
+    });
+bookRouter.route('/Single')
+    .get(function(req,res){
+        res.send('Hello Single Book');
+    });
 
+app.use('/Books', bookRouter);
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Render from handlebars', list:['Carmen', 'Gabriel', 'Jeremy']});
+    res.render('index',
+        {
+            title: 'Render from ejs',
+            nav:[{link: '/Books', text: 'Books'},{ link: '/Authors', text: 'Authors'} ]
+        });
 });
 app.listen(5000, function(err) {
     console.log('running server on port ' + port);
