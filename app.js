8d9c89d373547a8ce015fd8eefe5eff5ecef0f25
cde <cdechauri@gmail.com>
2016-04-29 09:34:59 -0700

    Add books through admin routes using mongodb

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-var bookRouter = express.Router();
+var nav = [{link: '/Books', text: 'Books'},{ link: '/Authors', text: 'Authors'} ];
+var bookRouter = require('./src/routes/bookRoutes')(nav);
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 
 
 app.use(express.static('public'));
@@ -11,16 +13,8 @@ app.set('views','./src/views');
 
 app.set('view engine','ejs');
 
-bookRouter.route('/')
-    .get(function(req,res){
-        res.send('Hello Books');
-    });
-bookRouter.route('/Single')
-    .get(function(req,res){
-        res.send('Hello Single Book');
-    });
-
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 app.get('/', function(req, res) {
     res.render('index',
         {
