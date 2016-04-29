365e3caa61041f783d5514a671034076dea3490b
cde <cdechauri@gmail.com>
2016-04-29 09:55:46 -0700

    Add auth route for authentication

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = process.env.PORT || 5000;
 var nav = [{link: '/Books', text: 'Books'},{ link: '/Authors', text: 'Authors'} ];
+
+//Routes
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 var adminRouter = require('./src/routes/adminRoutes')(nav);
+var authRouter = require('./src/routes/authRoutes')(nav);
 
-
+//Middleware
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
 app.set('views','./src/views');
 
 app.set('view engine','ejs');
 
 app.use('/Books', bookRouter);
 app.use('/Admin', adminRouter);
+app.use('/Auth', authRouter);
+
 app.get('/', function(req, res) {
     res.render('index',
         {
